7f9366c538c5411ed13524cefc6cdd926b8c553b
Rafaelsn11 <140290416+Rafaelsn11@users.noreply.github.com>
2023-12-04 12:12:37 -0300

    teste_e_adicoes_funcionalidades

--- model/cadastro_acompanhamento_individual.py
+++ model/cadastro_acompanhamento_individual.py
@@ -48,3 +48,25 @@ class CadastroAluno_Acompanhamento:
                 escritor = csv.writer(arquivo_csv)
                 escritor.writerow(lista_avaliacao_aluno)                                    
             
+    def relatorios(self,nome):
+        if os.path.isfile(f"database/avaliacao_alunos/{nome}.csv"):
+            count = 0
+            with open(f"database/avaliacao_alunos/{nome}.csv", "r") as arquivo_relatorios:
+                leitor = csv.reader(arquivo_relatorios)
+                next(leitor)
+                for avaliacao in leitor:
+                    if avaliacao[0] == nome:
+                        count += 1
+                        if count == 5:
+                            if os.path.isfile(f"database/relatorio_alunos/{nome}.csv"):
+                                with open(f"database/relatorio_alunos/{nome}.csv", "a") as arquivo_relatorio_nome:
+                                    escritor = csv.writer(arquivo_relatorio_nome)
+                                    escritor.writerow(avaliacao)
+                            else:
+                                with open(f"database/relatorio_alunos/{nome}.csv", "a", newline='') as arquivo_relatorio_nome:
+                                    escritor = csv.writer(arquivo_relatorio_nome)
+                                    escritor.writerow(['Nome','Data','Teste_Fisico','Peso','Desempenho_Fisico'])
+
+gr = CadastroAluno_Acompanhamento()
+gr.avaliacao_periodica("Rafael")
+gr.relatorios("Rafael")
