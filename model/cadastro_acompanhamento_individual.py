ac0b87e0b06b8211c7c7b12cc9ec610efde32cb5
Ott-on <ottonvinicius1004@gmail.com>
2023-12-04 11:05:15 -0300

    avaliacao_append

--- model/cadastro_acompanhamento_individual.py
+++ model/cadastro_acompanhamento_individual.py
@@ -24,12 +24,18 @@ class CadastroAluno_Acompanhamento:
         aluno.append(float(input("Altura: ")))
         aluno.append(input("Digite as metas de condicionamento do aluno (se houver):"))
         
-        with open(self.cadastro,'a',newline='') as arquivo_csv:
+        with open(self.cadastro,'a') as arquivo_csv:
             escritor = csv.writer(arquivo_csv,delimiter=';')
             escritor.writerow(aluno)
 
     def avaliacao_periodica(self,nome):
         lista_avaliacao_aluno = []
+
+        if not os.path.isfile(f"database/avaliacao_alunos/{nome}.csv"):
+            with open(f'database/avaliacao_alunos/{nome}.csv','a',newline='') as arquivo_csv:
+                escritor = csv.writer(arquivo_csv)
+                escritor.writerow(['Nome','Data','Teste_Fisico','Peso','Desempenho_Fisico'])   
+
         print("Avaliação do aluno")
         lista_avaliacao_aluno.append(nome)
         lista_avaliacao_aluno.append(input("Digite a data da avaliação: "))
@@ -38,8 +44,7 @@ class CadastroAluno_Acompanhamento:
         lista_avaliacao_aluno.append(input("Digite a análise de desempenho em exercícios (se houver): "))
         
         if os.path.isfile(f"database/avaliacao_alunos/{nome}.csv"):              
-            with open(f'database/avaliacao_alunos/{nome}.csv','a',newline='') as arquivo_csv:
+            with open(f'database/avaliacao_alunos/{nome}.csv','a') as arquivo_csv:
                 escritor = csv.writer(arquivo_csv)
                 escritor.writerow(lista_avaliacao_aluno)                                    
-
             
